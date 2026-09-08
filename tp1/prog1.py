76e89d77eab9233ff87a1d5b22244353a6f35451
iliesslm <iliesslm>
2026-09-08 14:57:48 +0200

    ajout des tests unitaires

--- tp1/prog1.py
+++ tp1/prog1.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+import fonctions as f
+
+while True:
+    a = int(input("Entrez le premier nombre : "))
+    b = int(input("Entrez le deuxième nombre : "))
+
+    res = f.puissance(a, b)
+
+    print("Résultat =", res)
